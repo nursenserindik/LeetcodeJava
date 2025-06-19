eea835b6b8541e54fc98c200e9652d4e8bb65577
nursenserindik <121162673+nursenserindik@users.noreply.github.com>
2025-06-19 21:57:58 +0300

    Update TwoSum.java

--- src/easy/TwoSum.java
+++ src/easy/TwoSum.java
@@ -5,29 +5,22 @@ import java.util.Arrays;
 public class TwoSum {
     public static int[] twoSum(int[] nums, int target) {
 
-
-        //target 9
-        // nums [2,7,11,15]
-
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) { //bir sonrakinden baslayıpp karsilastir
+            for (int j = i + 1; j < nums.length; j++) {
                 if (nums[j] == target - nums[i]) {
                     return new int[] { i, j };
                 }
             }
         }
-        return new int[] {}; //eslesme yoksa
+        return new int[] {};
     }
-    // Test
     public static void main(String[] args) {
-
         int[] numbers ={2,7,6,10};
         int target=9;
 
         int[] result = twoSum(numbers, target);
 
         System.out.println("Sonuç: " + Arrays.toString(result));
-
     }
 }
 
