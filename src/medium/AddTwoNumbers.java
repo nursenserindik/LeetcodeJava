8a2c5df527b2f1763311e2ab84ecf82f43bf70e7
nursenserindik <121162673+nursenserindik@users.noreply.github.com>
2025-06-19 21:56:57 +0300

    Update AddTwoNumbers.java

--- src/medium/AddTwoNumbers.java
+++ src/medium/AddTwoNumbers.java
@@ -1,45 +1,33 @@
 package medium;
 
 public class AddTwoNumbers {
-
-
     public class ListNode {
         int val;
         ListNode next;
 
         ListNode() {
         }
-
         ListNode(int val) {
             this.val = val;
         }
-
         ListNode(int val, ListNode next) {
             this.val = val;
             this.next = next;
         }
-
-
         class Solution {
             public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-                int val;     // Düğümdeki veri
-                ListNode next;  // Sonraki düğümü işaret et
+                int val;
+                ListNode next;
                 ListNode baslangc = new ListNode();
                 ListNode current = baslangc;
                 int carr = 0;
 
-                //current yeni liste takibi
-                //carr elde
-
-                while (l1 != null || l2 != null || carr != 0) { //  + eldenin bos olup olmadıgına bakmak
-
+                while (l1 != null || l2 != null || carr != 0) {
                     int sum = carr;
 
-
-                    if (l1 != null) { //bos degilse
-                        sum += l1.val; //toplama ekle
-                        l1 = l1.next; //sıradaki basamak
-
+                    if (l1 != null) {
+                        sum += l1.val;
+                        l1 = l1.next;
                     }
 
                     if (l2 != null) {
@@ -47,12 +35,11 @@ public class AddTwoNumbers {
                         l2 = l2.next;
                     }
 
-                    carr = sum / 10; //crr kontrol
+                    carr = sum / 10;
                     current.next = new ListNode(sum % 10);
                     current = current.next;
                 }
                 return baslangc.next;
-
             }
         }
     }
